baf4d0340319bb0a09f55763a0f8058d6131109e
Carlos Mertens <mertenscarlos@yahoo.com>
2021-05-19 00:53:14 +0200

    Refactor: CustomMap reusable

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,11 @@
-import { User } from './User';
-import { Company } from './Company';
+// "Instructions" to every other class on
+// how to be an argument to 'addMarker' method
+interface Pinpoint {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -14,22 +20,12 @@ export class CustomMap {
     });
   }
 
-  addUserMarker(user: User): void {
+  addMarker(pinpoint: Pinpoint): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
-      },
-    });
-  }
-
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
+        lat: pinpoint.location.lat,
+        lng: pinpoint.location.lng,
       },
     });
   }
